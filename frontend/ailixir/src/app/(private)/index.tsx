ae38804e611c229f890e8b7b110e33b81db15356
Hannes Vinzens <hannes.vinzens@fau.de>
2026-05-01 12:02:19 +0200

    refactor: remove unused boilerplate components and update AppBar
    - remove boilerplate components
    - simplify homescreen
    - update appbar to use correct icon
    
    Signed-off-by: Hannes Vinzens <hannes.vinzens@fau.de>

--- frontend/ailixir/src/app/(private)/index.tsx
+++ frontend/ailixir/src/app/(private)/index.tsx
@@ -1,25 +1,18 @@
 import { Link } from 'expo-router';
-import { Platform } from 'react-native';
 
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
 import { CText } from '@/components/atoms';
 
 export default function HomeScreen() {
   return (
-    <ThemedView>
-      <>
-        <CText fontSize={40}>Welcome to Ailixir!</CText>
+    <>
+      <CText fontSize={40}>Welcome to Ailixir!</CText>
 
-        <Link href="/documents/" asChild>
-          <CText>Go to Documents</CText>
-        </Link>
-        <Link href="/chats/" asChild>
-          <CText>Chats</CText>
-        </Link>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </>
-    </ThemedView>
+      <Link href="/documents/" asChild>
+        <CText>Go to Documents</CText>
+      </Link>
+      <Link href="/chats/" asChild>
+        <CText>Chats</CText>
+      </Link>
+    </>
   );
 }
--- frontend/ailixir/src/components/organisms/app-bar.tsx
+++ frontend/ailixir/src/components/organisms/app-bar.tsx
@@ -1,6 +1,7 @@
 import { CButton, CText } from '@/components/atoms';
-import { Circle, XStack } from 'tamagui';
+import { ChevronLeft } from '@tamagui/lucide-icons-2';
 import { useRouter } from 'expo-router';
+import { Circle, XStack } from 'tamagui';
 
 export function AppBar() {
   const router = useRouter();
@@ -8,10 +9,10 @@ export function AppBar() {
   return (
     <>
       <XStack gap={10} justify="space-between" width={300}>
+        <CButton icon={ChevronLeft} onPress={() => router.back()}></CButton>
         <CText fontSize={40} fontWeight="bold">
           AiLixir
         </CText>
-        <CButton onPress={() => router.back()}>Back</CButton>
         <Circle size={40} background="blue" />
       </XStack>
     </>
