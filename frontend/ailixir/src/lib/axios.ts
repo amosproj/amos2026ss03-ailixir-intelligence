3342eac4d584827ffd489a1cac2a4c47547f20fc
Hannes Vinzens <hannes.vinzens@fau.de>
2026-05-17 14:07:14 +0200

    feat: axios apiClient configuration
    Signed-off-by: Hannes Vinzens <hannes.vinzens@fau.de>

---
+++ frontend/ailixir/src/lib/axios.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { getAuth } from 'firebase/auth';
+
+const apiClient = axios.create({
+  baseURL: 'https://ailixir-backend-5mg2ellzaa-ue.a.run.app',
+});
+
+apiClient.interceptors.request.use(async (config) => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  if (user) {
+    const token = await user.getIdToken();
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default apiClient;
