e4dbf6d70f69ed6dc2d486bf8c9821b3cd1738c9
Hossam AMER <HOSSAMYASSERAMER@GMAIL.COM>
2026-05-17 14:11:51 +0200

    Set up worker CI/CD pipeline and grant Pub/Sub Cloud Run invoke access
    Signed-off-by: Hossam AMER <HOSSAMYASSERAMER@GMAIL.COM>

--- Backend/terraform/main.tf
+++ Backend/terraform/main.tf
@@ -44,6 +44,47 @@ resource "google_cloud_run_service_iam_member" "public" {
   member   = "allUsers"
 }
 
-output "service_url" {
-  value = google_cloud_run_service.backend.status[0].url
+resource "google_cloud_run_service" "worker" {
+  name     = "ailixir-worker"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/${var.project_id}/ailixir-worker:latest"
+        ports {
+          container_port = 8080
+        }
+        env {
+          name  = "GCP_PROJECT_ID"
+          value = var.project_id
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+# Grant Cloud Pub/Sub service account access to invoke the worker service
+resource "google_cloud_run_service_iam_member" "pubsub_invoker" {
+  service  = google_cloud_run_service.worker.name
+  location = google_cloud_run_service.worker.location
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:service-${data.google_client_config.current.project_number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
+data "google_client_config" "current" {}
+
+output "backend_service_url" {
+  value       = google_cloud_run_service.backend.status[0].url
+  description = "URL of the ailixir-backend service"
+}
+
+output "worker_service_url" {
+  value       = google_cloud_run_service.worker.status[0].url
+  description = "URL of the ailixir-worker service"
 }
