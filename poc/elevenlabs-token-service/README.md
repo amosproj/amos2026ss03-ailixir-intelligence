e35032897f9daf62da4a5dc75e9c4f2d4daaadc8
Hannes Vinzens <hannes.vinzens@fau.de>
2026-06-13 17:00:55 +0200

    feat: scaffold auth middleware
    Signed-off-by: Hannes Vinzens <hannes.vinzens@fau.de>

--- poc/elevenlabs-token-service/README.md
+++ poc/elevenlabs-token-service/README.md
@@ -1,8 +1,17 @@
+# Elevenlabs Token Provider
+
+## About
+
+This service answers incoming requests to the `/scribe-token`.
+It issues a token that is valid for 15 Minutes.
+
+## How to run 
+
 ```
 npm install
 npm run dev
 ```
 
 ```
-open http://localhost:3000
+open http://localhost:3000/scribe-token
 ```
--- poc/elevenlabs-token-service/src/index.ts
+++ poc/elevenlabs-token-service/src/index.ts
@@ -1,15 +1,33 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+// TODO: Implement firbase Auth token validation
+import { initializeApp } from "firebase-admin/app";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(async (c, next) => {
+  const authHeader = c.req.header("Authorization");
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    const token = authHeader.split(" ")[1];
+    console.log("token:", token);
+  } else {
+    console.log("No Bearer Token found");
+    return c.text("Unauthorized Request", 401);
+  }
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+  next();
+});
+
+app.get("/scribe-token", (c) => {
+  return c.json({ token: 1234 });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
